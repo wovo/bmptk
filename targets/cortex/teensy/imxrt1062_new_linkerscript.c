f63a2fe06af570b275f299613147619346e7a129
OscarKro <oscarkromhout@gmail.com>
2020-10-05 15:44:02 +0200

    added a new linker script (not usable yet), added new startup script (not usable yet), added a dummy stack (empty)

---
+++ targets/cortex/teensy/imxrt1062_new_linkerscript.c
@@ -0,0 +1,85 @@
+/**
+ * @file imxrt1062_new_linkerscript.ld
+ * @author Oscar Kromhout (https://github.com/OscarKro)
+ * @brief File containing the startup code for the IMXRT1062 chip on the Teensy 4.0 board
+ * @version 0.1
+ * @date 2020-10-01
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
+MEMORY /*This is the "map" of the memory allocation*/
+{
+	ITCM (rwx):  ORIGIN = 0x00000000, LENGTH = 512K
+	DTCM (rwx):  ORIGIN = 0x20000000, LENGTH = 512K
+	RAM (rwx):   ORIGIN = 0x20200000, LENGTH = 512K
+	FLASH (rwx): ORIGIN = 0x60000000, LENGTH = 1984K
+}
+
+ENTRY(image_vector_table) /* the place where the image starts */
+
+SECTIONS
+{
+    .text : {
+    KEEP(*(.flashconfig)) /* all the flash memory configuration data from startup.cpp */
+    . = ORIGIN(FLASH) + 0x1000; /* Jump to where we are going to store the vector table and such */
+   	KEEP(*(.IVT)) /* The image vector table from startup.cpp */
+    KEEP(*(.boot_data)) /* the boot data array from startup.cpp */
+    KEEP(*(.vector_table)) /* the actual vector table that would normally start on 0x0 in other cortexes from startup.cpp*/
+    KEEP(*(.start_of_code)) /* The start of all the code */
+	*(.start_utility) /* the code used by the startup code */
+	      . = ALIGN(4);
+                KEEP(*(.init))
+                __preinit_array_start = .;
+                KEEP(*(.preinit_array))
+                __preinit_array_end = .;
+                __init_array_start = .;
+                KEEP(*(.init_array))
+                __init_array_end = .;
+		. = ALIGN(16);
+    } > FLASH
+
+    /* This section was taken (and altered) from Paul Stoffregens linkerscript.*/
+    .itcm : {
+		. = . + 32; /* MPU to trap NULL pointer deref */
+		*(.text)
+		*(.text*)
+		. = ALIGN(16);
+	} > ITCM AT > FLASH /* let the virtual memory adress (ITCM), point to the load memory adress (FLASH) */
+
+    /* This section was taken (and not altered) from Paul Stoffregens linkerscript. Has something to do with ARM gnu compiler, not really sure what exactly.*/
+    .ARM.exidx : {
+		__exidx_start = .;
+		*(.ARM.exidx* .gnu.linkonce.armexidx.*)
+		__exidx_end = .;
+	} > ITCM AT > FLASH /*let the virtual memory adress (ITCM), point to the load memory adress (FLASH) */
+
+    /* This section was taken (and altered) from Paul Stoffregens linkerscript. This section holds all the initialized variables*/
+    .data : {
+		*(.rodata)
+		*(.data)
+		. = ALIGN(16);
+	} > DTCM AT > FLASH /*let the virtual memory adress (DTCM), point to the load memory adress (FLASH) */
+
+    /* This section was taken (and  altered) from Paul Stoffregens linkerscript. This section holds all the uninitialized variables*/
+	.bss ALIGN(4) : {
+		. = ALIGN(4); /* align on four bytes, got this from the standard bmpk_linkerscript_cortex */
+		KEEP(*(.bmptk_stack)) /* bmptk_stack to be placed here */
+		*(.bss*)
+		*(COMMON)
+		. = ALIGN(32);
+		. = . + 32; /* MPU to trap stack overflow */
+	} > DTCM 
+}
+PROVIDE(size_of_image = SIZEOF(.text) + SIZEOF(.itcm) + SIZEOF(.ARM.exidx) + SIZEOF(.data)); /*Set the size of the image in this variable, this is used by the startup.cpp script*/
+PROVIDE(start_adress_of_bss = ADDR(.bss)); /*Get the start adress of the bss, is used by the init_bss_zero function in startup.cpp */
+PROVIDE(end_adress_of_bss = ADDR(.bss) + SIZEOF(.bss)); /*Get the end adress of the bss, is used by the init_bss_zero function in startup.cpp */
+PROVIDE(start_adress_of_text = ADDR(.itcm)); /* get the start adress of the text block */
+PROVIDE(end_adress_of_text = ADDR(.itcm) + SIZEOF(.itcm) + SIZEOF(.ARM.exidx)); /* get the end of the text block */
+PROVIDE(load_adress_of_text = LOADADDR(.itcm)); /* get the load adress (because of the at keyword) from the text block */
+PROVIDE(start_adress_of_data = ADDR(.data)); /* get the start adress of the data section*/
+PROVIDE(end_adress_of_data = ADDR(.data) + SIZEOF(.data)); /*get the end adress of the data section*/
+PROVIDE(load_adress_of_data = LOADADDR(.data)); /*get the load adress (because of the at keyword) from the data block*/
+PROVIDE(stack_start_adress = ADDR(.bmptk_stack)); /* get the stack start adress */
+PROVIDE(stack_end_adress = ADDR(.bmptk_stack) + SIZEOF(.bmptk_stack)); /* get the stack end adress */
---
+++ targets/cortex/teensy/startup.cpp
@@ -0,0 +1,323 @@
+/**
+ * @file startup.cpp
+ * @author Oscar Kromhout (https://github.com/OscarKro)
+ * @brief File containing the startup code for the IMXRT1062 chip on the Teensy 4.0 board
+ * @version 0.1
+ * @date 2020-10-01
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+#include <stdint.h>
+#include "mimxrt1062.h"
+
+// forward declaration for a main
+int main(void);
+// forward declaration for a start
+void start();
+// forward declaration of the size of our image, used in the boot_data array
+extern uint32_t size_of_image;
+// forward declaration of the adress of the start of the text block (From the linker script)
+extern uint32_t start_adress_of_text;
+// forward declaration of the adress of the end of the text block (From the linker script)
+extern uint32_t end_adress_of_text;
+// forward declaration of the load adress of the text block
+extern uint32_t load_adress_of_text;
+// forward declaration of the adress of the bss (from the linker script)
+extern uint32_t start_adress_of_bss;
+// forward declaration of the size of the bss (from the linker script)
+extern uint32_t end_adress_of_bss;
+// forward declaration of the adress of the start of the data block (From the linker script)
+extern uint32_t start_adress_of_data;
+// forward declaration of the adress of the end of the data block (From the linker script)
+extern uint32_t end_adress_of_data;
+// forward declaration of the load adress of the data block
+extern uint32_t load_adress_of_data;
+// forward declaration of the stack start adress
+extern uint32_t stack_start_adress;
+// forward declaration of the stack end adress
+extern uint32_t stack_end_adress;
+
+__attribute__ ((section(".vector_table"), used)) // compiler tag .vector_table
+/**
+ * @brief The vector table containing the first start adress
+ * 
+ */
+uint32_t vector_table[2] =
+	{
+		(uint32_t)stack_end_adress,
+		(unsigned int)&start // adress to the start up function. First image adress (pc)
+};
+
+__attribute__ ((section(".boot_data"), used)) // compiler tag .boot_data
+/**
+ * @brief The boot data vector. p. 262
+ * 
+ */
+uint32_t boot_data[3] =
+	{
+		0x60000000,	   // Needs to be the first adress for the image
+		size_of_image, // size of the total image
+		0			   // some plugin image?
+};
+
+__attribute__ ((section(".IVT"), used)) // compiler tag .IVT (image vector table)
+/**
+ * @brief The Image Vector Table (IVT). Vector containing the most basic data adresses. p. 216
+ * 
+ */
+uint32_t image_vector_table[8] =
+	{
+		0x402000D1,					  // Header adress, found in bootdata.c from arduino code. this is what paul stoffregen does, dont know why. p. 262 has short description
+		(uint32_t)vector_table,	  // the docs are apparently wrong here, this should not be the first image adress, but the adress to the actual vector table
+		0,							  // reserved and should be zero
+		0,							  // reserved for absolute adress for the device configuration data
+		(uint32_t)boot_data,							  // reserved for the absolute adress for the boot data
+		(uint32_t)image_vector_table, // Absolute address of the IVT. Used internally by the ROM
+		0,							  // Absolute address of the Command Sequence File (CSF) used by the HAB library
+		0							  // Reserved
+};									  // p. 260
+
+__attribute__ ((section(".flashconfig"), used)) // compiler tag .flashconfig
+/**
+ * @brief This array was used from Paul Stoffregen's bootdata.c. It contains all the boot information for the chip and is placed on adress 0.
+ * 
+ */
+uint32_t FlexSPI_NOR_Config[128] = {
+	// 448 byte common FlexSPI configuration block, 8.6.3.1 page 223 (RT1060 rev 0)
+	// MCU_Flashloader_Reference_Manual.pdf, 8.2.1, Table 8-2, page 72-75
+	0x42464346, // Tag				0x00
+	0x56010000, // Version
+	0,			// reserved
+	0x00020101, // columnAdressWidth,dataSetupTime,dataHoldTime,readSampleClksource
+
+	0x00000000, // waitTimeCfgCommands,-,deviceModeCfgEnable
+	0,			// deviceModeSeq
+	0,			// deviceModeArg
+	0x00000000, // -,-,-,configCmdEnable
+
+	0, // configCmdSeqs		0x20
+	0,
+	0,
+	0,
+
+	0, // cfgCmdArgs			0x30
+	0,
+	0,
+	0,
+
+	0x00000000, // controllerMiscOption		0x40
+	0x00030401, // lutCustomSeqEnable,serialClkFreq,sflashPadType,deviceType
+	0,			// reserved
+	0,			// reserved
+	0x00200000, // sflashA1Size			0x50
+	0,			// sflashA2Size
+	0,			// sflashB1Size
+	0,			// sflashB2Size
+
+	0, // csPadSettingOverride		0x60
+	0, // sclkPadSettingOverride
+	0, // dataPadSettingOverride
+	0, // dqsPadSettingOverride
+
+	0,			// timeoutInMs			0x70
+	0,			// commandInterval
+	0,			// dataValidTime
+	0x00000000, // busyBitPolarity,busyOffset
+
+	0x0A1804EB, // lookupTable[0]		0x80
+	0x26043206, // lookupTable[1]
+	0,			// lookupTable[2]
+	0,			// lookupTable[3]
+
+	0x24040405, // lookupTable[4]		0x90
+	0,			// lookupTable[5]
+	0,			// lookupTable[6]
+	0,			// lookupTable[7]
+
+	0, // lookupTable[8]		0xA0
+	0, // lookupTable[9]
+	0, // lookupTable[10]
+	0, // lookupTable[11]
+
+	0x00000406, // lookupTable[12]		0xB0
+	0,			// lookupTable[13]
+	0,			// lookupTable[14]
+	0,			// lookupTable[15]
+
+	0, // lookupTable[16]		0xC0
+	0, // lookupTable[17]
+	0, // lookupTable[18]
+	0, // lookupTable[19]
+
+	0x08180420, // lookupTable[20]		0xD0
+	0,			// lookupTable[21]
+	0,			// lookupTable[22]
+	0,			// lookupTable[23]
+
+	0, // lookupTable[24]		0xE0
+	0, // lookupTable[25]
+	0, // lookupTable[26]
+	0, // lookupTable[27]
+
+	0, // lookupTable[28]		0xF0
+	0, // lookupTable[29]
+	0, // lookupTable[30]
+	0, // lookupTable[31]
+
+	0x081804D8, // lookupTable[32]		0x100
+	0,			// lookupTable[33]
+	0,			// lookupTable[34]
+	0,			// lookupTable[35]
+
+	0x08180402, // lookupTable[36]		0x110
+	0x00002004, // lookupTable[37]
+	0,			// lookupTable[38]
+	0,			// lookupTable[39]
+
+	0, // lookupTable[40]		0x120
+	0, // lookupTable[41]
+	0, // lookupTable[42]
+	0, // lookupTable[43]
+
+	0x00000460, // lookupTable[44]		0x130
+	0,			// lookupTable[45]
+	0,			// lookupTable[46]
+	0,			// lookupTable[47]
+
+	0, // lookupTable[48]		0x140
+	0, // lookupTable[49]
+	0, // lookupTable[50]
+	0, // lookupTable[51]
+
+	0, // lookupTable[52]		0x150
+	0, // lookupTable[53]
+	0, // lookupTable[54]
+	0, // lookupTable[55]
+
+	0, // lookupTable[56]		0x160
+	0, // lookupTable[57]
+	0, // lookupTable[58]
+	0, // lookupTable[59]
+
+	0, // lookupTable[60]		0x170
+	0, // lookupTable[61]
+	0, // lookupTable[62]
+	0, // lookupTable[63]
+
+	0, // LUT 0: Read			0x180
+	0, // LUT 1: ReadStatus
+	0, // LUT 3: WriteEnable
+	0, // LUT 5: EraseSector
+
+	0, // LUT 9: PageProgram		0x190
+	0, // LUT 11: ChipErase
+	0, // LUT 15: Dummy
+	0, // LUT unused?
+
+	0, // LUT unused?			0x1A0
+	0, // LUT unused?
+	0, // LUT unused?
+	0, // LUT unused?
+
+	0, // reserved			0x1B0
+	0, // reserved
+	0, // reserved
+	0, // reserved
+
+	// 64 byte Serial NOR configuration block, 8.6.3.2, page 346
+
+	256,  // pageSize			0x1C0
+	4096, // sectorSize
+	1,	  // ipCmdSerialClkFreq
+	0,	  // reserved
+
+	0x00010000, // block size			0x1D0
+	0,			// reserved
+	0,			// reserved
+	0,			// reserved
+
+	0, // reserved			0x1E0
+	0, // reserved
+	0, // reserved
+	0, // reserved
+
+	0, // reserved			0x1F0
+	0, // reserved
+	0, // reserved
+	0  // reserved
+};
+
+__attribute__ ((section(".start_utility"), used))
+/**
+ * @brief Function to initialize the complete bss with zero
+ * 
+ */
+void
+init_bss_zero()
+{
+	uint32_t *d, *e;
+
+	// clear .bss section
+	d = &start_adress_of_bss;
+	e = &end_adress_of_bss;
+	while (d != e)
+	{
+		*d++ = 0;
+	}
+}
+
+__attribute__ ((section(".start_utility"), used))
+/**
+ * @brief Function to copy memory 
+ * 
+ * @param source the source from where memory needs to be copied (first memory adress)	
+ * @param destination the destination to copy to (first memory adress)
+ * @param end_destination the end adress of the memory block (last memory adress +1 )
+ */
+void
+copy_memory(uint32_t *source, uint32_t *destination, uint32_t *end_destination)
+{
+	if (destination == source)
+		return;
+	while (destination < end_destination)
+	{
+		*destination++ = *source++; // copy the number at source adress to destination adress, than add 1 to both adresses
+	}
+}
+
+
+__attribute__ ((section(".start_of_code"), used))
+/**
+ * @brief This function is the entry point for the linkerscript
+ * 
+ */
+void
+start()
+{
+	// set the bss to zero
+	init_bss_zero();
+	// copy the text memory to itcm
+	copy_memory(&start_adress_of_text, &load_adress_of_text, &end_adress_of_text);
+	// copy the data memory to dtcm
+	copy_memory(&start_adress_of_data, &load_adress_of_data, &end_adress_of_data);
+
+	//stuff to turn the light on
+	IOMUXC->SW_MUX_CTL_PAD[kIOMUXC_SW_MUX_CTL_PAD_GPIO_B0_03] |= 5;
+    IOMUXC_GPR->GPR27 |= 0xFFFFFFFF;
+    GPIO7->GDIR |= (1 << 3);
+    GPIO7->DR_SET |= (1 << 3);
+
+	(void)main(); // call to main from hwlib
+
+	// when returned from the main, loop till hell freezes over, but do something to eliminate unidentified behaviour
+	volatile int a = 0;
+	while (1)
+	{
+		if (a == 100000)
+		{
+			a = 0;
+		}
+		a++;
+	}
+}
