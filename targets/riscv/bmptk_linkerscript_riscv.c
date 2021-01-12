1a51436bc6bb725e28144c18054ff99428d07a4f
wovo <wouter@voti.nl>
2021-01-12 14:06:53 +0100

    update

--- targets/riscv/bmptk_linkerscript_riscv.c
+++ targets/riscv/bmptk_linkerscript_riscv.c
@@ -1,257 +1,171 @@
-/******************************************************************************
-* author: Freddie Chopin, http://www.freddiechopin.info/
-* file: lpc1114_rom.ld
-* last change: 2010-10-13
-*
-* chip: LPC1114
-* compiler: arm-none-eabi-gcc (Sourcery G++ Lite 2010q1-188) 4.4.1
-*
-* description:
-* Linker script for LPC1114 chip (/301 version) (32kB Flash, 4kB SRAM).
-******************************************************************************/
+OUTPUT_ARCH( "riscv" )
 
-SEARCH_DIR(.);
-
-/*
-+=============================================================================+
-| format configurations
-+=============================================================================+
-*/
-
-OUTPUT_FORMAT("elf32-littlearm", "elf32-bigarm", "elf32-littlearm");
-OUTPUT_ARCH(arm);
-
-
-/*
-+=============================================================================+
-| available memories definitions
-+=============================================================================+
-*/
+ENTRY( _start )
 
 MEMORY
-{
-	rom (rx)  	: org = ROM_START, len = ROM_SIZE
-	ram (rwx)	: org = RAM_START, len = RAM_SIZE
-	nul (rwx)	: org = RAM_START + RAM_SIZE, len = 0k
+{ 
+	/* Run in FLASH */ 
+	flash (rxai!w) : ORIGIN = 0x08000000, LENGTH = 32k
+	ram   (wxa!ri) : ORIGIN = 0x20000000, LENGTH = 10k 
+
 }
 
-__rom_start = ORIGIN(rom);
-__rom_size = LENGTH(rom);
-__rom_end = __rom_start + __rom_size;
-
-__ram_start = ORIGIN(ram);
-__ram_size = LENGTH(ram);
-__ram_end = __ram_start + __ram_size;
-
-PROVIDE(__rom_start = __rom_start);
-PROVIDE(__rom_size = __rom_size);
-PROVIDE(__rom_end = __rom_end);
-
-PROVIDE(__ram_start = __ram_start);
-PROVIDE(__ram_size = __ram_size);
-PROVIDE(__ram_end = __ram_end);
-
-/*
-+=============================================================================+
-| entry point
-+=============================================================================+
-*/
-
-ENTRY( __startup );
-
-/*
-+=============================================================================+
-| put data in sections
-+=============================================================================+
-*/
 
 SECTIONS
 {
-	.text :
-	{
-		. = ALIGN(4);
-		__text_start = .;
-		PROVIDE(__text_start = __text_start);
-
-		. = ALIGN(4);
-		KEEP(*(.vectors));
-		. = ALIGN(4);
-		*(.text .text.* .gnu.linkonce.t.*);
-		. = ALIGN(4);
-		*(.glue_7t .glue_7);
-		. = ALIGN(4);
-		*(.rodata .rodata.* .gnu.linkonce.r.*);
-
-		. = ALIGN(4);
-		*(.ARM.extab* .gnu.linkonce.armextab.*);	/* exception unwinding information */
-		. = ALIGN(4);
-		*(.gcc_except_table);						/* information used for stack unwinding during exception */
-		. = ALIGN(4);
-		*(.eh_frame_hdr);							/* additional information about .ex_frame section */
-		. = ALIGN(4);
-		*(.eh_frame);								/* information used for stack unwinding during exception */
-
-/*
-		. = ALIGN(4);
-		KEEP(*(.init));
-		. = ALIGN(4);
-		__preinit_array_start = .;
-		KEEP(*(.preinit_array));
-		. = ALIGN(4);
-		__preinit_array_end = .;
-		__init_array_start = .;
-		KEEP(*(SORT(.init_array.*)));
-		. = ALIGN(4);
-		KEEP(*(.init_array));
-		. = ALIGN(4);
-		__init_array_end = .;
-*/
+  __stack_size = DEFINED(__stack_size) ? __stack_size : 1K;
 
 
-/*
-		KEEP(*(.fini));
-		. = ALIGN(4);
-		__fini_array_start = .;
-		KEEP(*(.fini_array));
-		. = ALIGN(4);
-		KEEP(*(SORT(.fini_array.*)));
-		. = ALIGN(4);
-		__fini_array_end = .;
-*/
-		. = ALIGN(4);
-		__text_end = .;
-		PROVIDE(__text_end = __text_end);
-	} > rom AT > rom
+  .init           :
+  {
+    KEEP (*(SORT_NONE(.init)))
+  } >flash AT>flash 
 
-	.IF_YOU_SEE_THIS_YOU_HAVE_ONE_OR_MORE_GLOBAL_OBJECT_CONSTRUCTORS :
-	{
-		KEEP(*(.init));
-		KEEP(*(.preinit_array));
-		KEEP(*(SORT(.init_array.*)));
-		KEEP(*(.init_array));
-	} > nul
+  .ilalign         :
+  {
+    . = ALIGN(4);
+    PROVIDE( _ilm_lma = . );
+  } >flash AT>flash 
 
+  .ialign         :
+  {
+    PROVIDE( _ilm = . );
+  } >flash AT>flash 
 
-	. = ALIGN(4);
-	__exidx_start = .;
-	PROVIDE(__exidx_start = __exidx_start);
+  .text           :
+  {
+    *(.rodata .rodata.*)  
+    *(.text.unlikely .text.unlikely.*)
+    *(.text.startup .text.startup.*)
+    *(.text .text.*)
+    *(.gnu.linkonce.t.*)
+  } >flash AT>flash 
 
-	.ARM.exidx :
-	{
-		*(.ARM.exidx* .gnu.linkonce.armexidx.*);
-	} > rom
+  .fini           :
+  {
+    KEEP (*(SORT_NONE(.fini)))
+  } >flash AT>flash 
 
-	. = ALIGN(4);
-	__exidx_end = .;
-	PROVIDE(__exidx_end = __exidx_end);
+  . = ALIGN(4);
 
-	.bss (NOLOAD) :
-	{
+  PROVIDE (__etext = .);
+  PROVIDE (_etext = .);/*0x80022c8*/
+  PROVIDE (etext = .);/*0x80022c8*/
+  PROVIDE( _eilm = . );
 
-		. = ALIGN(4);
-		__stack_start = .;
-		PROVIDE(__stack_start = __stack_start);
+  .preinit_array  :
+  {
+    PROVIDE_HIDDEN (__preinit_array_start = .);
+    KEEP (*(.preinit_array))
+    PROVIDE_HIDDEN (__preinit_array_end = .);
+  } >flash AT>flash 
 
-		. = ALIGN(4);
-		KEEP(*(.bmptk_stack));
+  .init_array     :
+  {
+    PROVIDE_HIDDEN (__init_array_start = .);
+    KEEP (*(SORT_BY_INIT_PRIORITY(.init_array.*) SORT_BY_INIT_PRIORITY(.ctors.*)))
+    KEEP (*(.init_array EXCLUDE_FILE (*crtbegin.o *crtbegin?.o *crtend.o *crtend?.o ) .ctors))
+    PROVIDE_HIDDEN (__init_array_end = .);
+  } >flash AT>flash 
 
-		. = ALIGN(8);
-		__main_stack_end = .;
-		PROVIDE(__main_stack_end = __main_stack_end);
+  .fini_array     :
+  {
+    PROVIDE_HIDDEN (__fini_array_start = .);
+    KEEP (*(SORT_BY_INIT_PRIORITY(.fini_array.*) SORT_BY_INIT_PRIORITY(.dtors.*)))
+    KEEP (*(.fini_array EXCLUDE_FILE (*crtbegin.o *crtbegin?.o *crtend.o *crtend?.o ) .dtors))
+    PROVIDE_HIDDEN (__fini_array_end = .);
+  } >flash AT>flash 
 
-		. = ALIGN(4);
-		__stack_end = .;
+  .ctors          :
+  {
+    /* gcc uses crtbegin.o to find the start of
+       the constructors, so we make sure it is
+       first.  Because this is a wildcard, it
+       doesn't matter if the user does not
+       actually link against crtbegin.o; the
+       linker won't look for a file to match a
+       wildcard.  The wildcard also means that it
+       doesn't matter which directory crtbegin.o
+       is in.  */
+    KEEP (*crtbegin.o(.ctors))
+    KEEP (*crtbegin?.o(.ctors))
+    /* We don't want to include the .ctor section from
+       the crtend.o file until after the sorted ctors.
+       The .ctor section from the crtend file contains the
+       end of ctors marker and it must be last */
+    KEEP (*(EXCLUDE_FILE (*crtend.o *crtend?.o ) .ctors))
+    KEEP (*(SORT(.ctors.*)))
+    KEEP (*(.ctors))
+  } >flash AT>flash 
 
-		PROVIDE(__stack_end = __stack_end);
+  .dtors          :
+  {
+    KEEP (*crtbegin.o(.dtors))
+    KEEP (*crtbegin?.o(.dtors))
+    KEEP (*(EXCLUDE_FILE (*crtend.o *crtend?.o ) .dtors))
+    KEEP (*(SORT(.dtors.*)))
+    KEEP (*(.dtors))
+  } >flash AT>flash 
 
-		. = ALIGN(4);
-		__bss_start = .;
-		PROVIDE(__bss_start = __bss_start);
+    . = ALIGN(4);
+    PROVIDE( _eilm = . );
 
-		. = ALIGN(4);
-		*(.bss .bss.* .gnu.linkonce.b.*)
-		. = ALIGN(4);
-		*(COMMON);
+  .lalign         :
+  {
+    . = ALIGN(4);
+    PROVIDE( _data_lma = . );
+  } >flash AT>flash 
 
-		. = ALIGN(4);
-		__bss_end = .;
-		PROVIDE(__bss_end = __bss_end);
-	} > ram
+  .dalign         :
+  {
+    . = ALIGN(4);
+    PROVIDE( _data = . );
+  } >ram AT>flash 
+  
+  
+  .data          :
+  {
+    *(.rdata) 
+   
+    *(.gnu.linkonce.r.*)
+    *(.data .data.*)
+    *(.gnu.linkonce.d.*)
+    . = ALIGN(8);
+    PROVIDE( __global_pointer$ = . + 0x800); 
+    *(.sdata .sdata.*)
+    *(.gnu.linkonce.s.*)
+    . = ALIGN(8);
+    *(.srodata.cst16)
+    *(.srodata.cst8)
+    *(.srodata.cst4)
+    *(.srodata.cst2)
+    *(.srodata .srodata.*)
+  } >ram AT>flash 
 
-	.data :
-	{
-		. = ALIGN(4);
-		__data_init_start = LOADADDR (.data);
-		PROVIDE(__data_init_start = __data_init_start);
-		__data_start = .;
-		PROVIDE(__data_start = __data_start);
+  . = ALIGN(4);
+  PROVIDE( _edata = . );
+  PROVIDE( edata = . );
 
-		. = ALIGN(4);
-		*(.data .data.* .gnu.linkonce.d.*)
+  PROVIDE( _fbss = . ); /*0X200052A0  0X200002A0*/
+  PROVIDE( __bss_start = . );
+  .bss            :
+  {
+    *(.sbss*)
+    *(.gnu.linkonce.sb.*)
+    *(.bss .bss.*)
+    *(.gnu.linkonce.b.*)
+    *(COMMON)
+    . = ALIGN(4);
+  } >ram AT>ram 
 
-		. = ALIGN(4);
-		__data_end = .;
-		PROVIDE(__data_end = __data_end);
+  . = ALIGN(8);
+  PROVIDE( _end = . ); /*0X2000,0340*/
+  PROVIDE( end = . );
 
-      __data_init_end = LOADADDR (.data);
-      PROVIDE( __data_init_end = __data_init_end );
-
-	} > ram AT > rom
-
-PROVIDE( __data_size = __data_end - __data_start);
-
-PROVIDE( __data_init_size = __data_size );
-PROVIDE( __data_init_end = __data_init_start + __data_init_size );
-
-
-	. = ALIGN(4);
-	__heap_start = .;
-	PROVIDE(__heap_start = __heap_start);
-
-	. = ALIGN(4);
-	__heap_end = __ram_end;
-	PROVIDE(__heap_end = __heap_end);
-
-	.stab 				0 (NOLOAD) : { *(.stab) }
-	.stabstr 			0 (NOLOAD) : { *(.stabstr) }
-	/* DWARF debug sections.
-	* Symbols in the DWARF debugging sections are relative to the beginning
-	* of the section so we begin them at 0. */
-	/* DWARF 1 */
-	.debug				0 : { *(.debug) }
-	.line				0 : { *(.line) }
-	/* GNU DWARF 1 extensions */
-	.debug_srcinfo		0 : { *(.debug_srcinfo) }
-	.debug_sfnames		0 : { *(.debug_sfnames) }
-	/* DWARF 1.1 and DWARF 2 */
-	.debug_aranges		0 : { *(.debug_aranges) }
-	.debug_pubnames		0 : { *(.debug_pubnames) }
-	/* DWARF 2 */
-	.debug_info			0 : { *(.debug_info .gnu.linkonce.wi.*) }
-	.debug_abbrev		0 : { *(.debug_abbrev) }
-	.debug_line			0 : { *(.debug_line) }
-	.debug_frame		0 : { *(.debug_frame) }
-	.debug_str			0 : { *(.debug_str) }
-	.debug_loc			0 : { *(.debug_loc) }
-	.debug_macinfo		0 : { *(.debug_macinfo) }
-	/* SGI/MIPS DWARF 2 extensions */
-	.debug_weaknames	0 : { *(.debug_weaknames) }
-	.debug_funcnames	0 : { *(.debug_funcnames) }
-	.debug_typenames	0 : { *(.debug_typenames) }
-	.debug_varnames		0 : { *(.debug_varnames) }
-
-	.note.gnu.arm.ident	0 : { KEEP(*(.note.gnu.arm.ident)) }
-	.ARM.attributes		0 : { KEEP(*(.ARM.attributes)) }
-	/DISCARD/				: { *(.note.GNU-stack) }
+  .stack ORIGIN(ram) + LENGTH(ram) - __stack_size :
+  {
+    PROVIDE( _heap_end = . ); 
+    . = __stack_size;  
+    PROVIDE( _sp = . ); 
+  } >ram AT>ram 
 }
-
-PROVIDE(__text_size = __text_end - __text_start);
-PROVIDE(__exidx_size = __exidx_end - __exidx_start);
-
-PROVIDE(__bss_size = __bss_end - __bss_start);
-PROVIDE(__stack_size = __stack_end - __stack_start);
-PROVIDE(__heap_size = __heap_end - __heap_start);
-
-/******************************************************************************
-* END OF FILE
-******************************************************************************/
